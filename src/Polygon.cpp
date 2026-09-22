c10f4411e0f059f54d77e0779da316ff16ee4e2c
SofianeLatr <soufianelatr@gmail.com>
2026-09-22 15:28:58 +0100

    use diffrent updatePoints function to update rect positions for the cpp file

--- src/Polygon.cpp
+++ src/Polygon.cpp
@@ -1,37 +1,38 @@
 #include "Polygon.hpp"
+#include <vector>
 
-template<int N>
-Polygon<N>::~Polygon() {};
-template<int N>
-Polygon<N>::Polygon(Vec2 pos, Vec2 size, float angle, float mass, bool isStatic) : Body(pos, angle, mass, isStatic) {};
-Rect::Rect(Vec2 pos, Vec2 size, float angle, float mass, bool isStatic) : Polygon<4>(pos, size, angle, mass, isStatic), size(size) {
+Polygon::~Polygon() {};
+Polygon::Polygon(Vec2 pos, Vec2 size, float angle, float mass, bool isStatic) : Body(pos, angle, mass, isStatic) {};
+Rect::Rect(Vec2 pos, Vec2 size, float angle, float mass, bool isStatic) : Polygon(pos, size, angle, mass, isStatic), size(size) {
     this->getPoints();
     inertia = this->getInertia();
 };
 
-template<int N>
-const std::array<Vec2, N>& Polygon<N>::getPoints()  {
+const std::vector<Vec2>& Polygon::getPoints()  {
     return points;
 }
 
-const std::array<Vec2, 4>& Rect::getPoints()  {
-    points[0] = Vec2(-size.x / 2,-size.y / 2);
-    points[1] = Vec2(+size.x / 2,-size.y / 2);
-    points[2] = Vec2(+size.x / 2,+size.y / 2);
-    points[3] = Vec2(-size.x / 2,+size.y / 2);
-    
-
-    for(int i = 0; i < 4; i++) {
-        points[i] = Vec2(points[i].x * cos(angle) - points[i].y * sin(angle), 
-                         points[i].x * sin(angle) + points[i].y * cos(angle));
+const std::vector<Vec2>& Rect::updatePoints()
+{
+    if (points.empty())
+    {
+        points.push_back(Vec2(-size.x / 2, -size.y / 2));
+        points.push_back(Vec2( size.x / 2, -size.y / 2));
+        points.push_back(Vec2( size.x / 2,  size.y / 2));
+        points.push_back(Vec2(-size.x / 2,  size.y / 2));
     }
 
-    points[0] += Vec2(pos.x,pos.y);
-    points[1] += Vec2(pos.x,pos.y);
-    points[2] += Vec2(pos.x,pos.y);
-    points[3] += Vec2(pos.x,pos.y);    
+    for (int i = 0; i < 4; i++)
+    {
+        Vec2 local = points[i];
 
+        points[i] = Vec2(
+            local.x * cos(angle) - local.y * sin(angle),
+            local.x * sin(angle) + local.y * cos(angle)
+        );
 
+        points[i] += pos;
+    }
 
     return points;
 }
